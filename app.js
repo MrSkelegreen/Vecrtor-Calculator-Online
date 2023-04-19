09696d55cbcea04db0a498a3b19d73a94ea2101c
MrSkelegreen <104663261+MrSkelegreen@users.noreply.github.com>
2023-04-19 17:13:38 +0700

    angle added

--- app.js
+++ app.js
@@ -46,3 +46,30 @@ function getdiff()
 
     return false;
 }
+
+function getScalarmult()
+{
+    var vars = new Vars();
+
+    var res = (vars.a1 * vars.b1) + (vars.a2 * vars.b2) + (vars.a3 * vars.b3);
+
+    return res;
+}
+
+function getangle()
+{
+    var vars = new Vars();
+
+    var scalarmultres = getScalarmult();
+
+    var absA = Math.sqrt(Math.pow(vars.a1,2) + Math.pow(vars.a2,2)+ Math.pow(vars.a3,2));
+    var absB = Math.sqrt(Math.pow(vars.b1,2) + Math.pow(vars.b2,2)+ Math.pow(vars.b3,2));
+
+    var cosA = scalarmultres / (absA * absB);
+
+    var res = Math.acos(cosA) * 57.3;
+
+    document.getElementById("answer").value = res.toFixed(2);
+
+    return false;
+}
